6318d9397cf85d0042e32e2ea264551b0b7b879b
John <johnitolee@gmail.com>
2017-05-07 14:07:20 -0700

    initial commit

---
+++ frontend/components/root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Root extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <div>hi</div>
+      </div>
+    );
+  }
+}
+
+export default Root;
