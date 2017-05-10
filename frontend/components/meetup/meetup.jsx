a692bf73754626b2f60c19f46b51c398ee56aa8e
John <johnitolee@gmail.com>
2017-05-10 16:51:25 -0700

    working on rendering events

--- frontend/components/meetup/meetup.jsx
+++ frontend/components/meetup/meetup.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Meetup extends Component{
   constructor(props){
     super(props);
+    this.rows = [];
   }
 
   componentDidMount(){
@@ -10,11 +11,27 @@ class Meetup extends Component{
     this.props.fetchMeetups(request);
   }
 
+  parseMeetups(){
+    // console.log('here');
+    // for(let i = 0; i < 10; i++){
+    //   this.rows.push(this.props.meetups.results[i]);
+    // }
+
+  }
+
   render(){
+    console.log(this.props);
+    let row;
+    if (Object.keys(this.props.meetups).length !== 0){
+      row = this.props.meetups.results.slice(0, 10);
+    }
+    console.log(row);
     return(
       <div>
         <div>
-          
+          {row.map(el => (
+            <li>el.name</li>
+          ))}
         </div>
       </div>
     );
