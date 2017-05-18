04337802e1ee56d68c9998b6b775f4202ff57267
Scott Heng <scottheng3@gmail.com>
2017-05-18 10:50:24 -0700

    fix module resolve issues

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MeetupContainer from './meetup/meetup_container';
-import EventbriteContainer from './eventbrite/eventbrite_container';
+import EventbriteContainer from './eventbrite/eventbrite_index_container';
 
 const App = (props) => {
   return (
