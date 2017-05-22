ace6b267b543e52431368be8eb679473cd9aa718
John <johnitolee@gmail.com>
2017-05-22 15:24:15 -0700

    add search container

---
+++ frontend/components/search/search_container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import Search from './search';
+import { fetchMeetups } from '../../actions/meetup_actions';
+import { fetchEbEvents } from '../../actions/eventbrite_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    meetups: state.meetup.data,
+    eventbrite: state.eventbrite.data
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  fetchMeetups: (request) => dispatch(fetchMeetups(request)),
+  fetchEbEvents: (request) => dispatch(fetchEbEvents(request))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
