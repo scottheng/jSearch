0225f6f40522003757ad01ecc9bef4c0f1ca9604
Scott Heng <scottheng3@gmail.com>
2017-05-22 15:27:05 -0700

    add search container

--- frontend/utils/meetup_api.js
+++ frontend/utils/meetup_api.js
@@ -1,37 +1,38 @@
 import axios from 'axios';
 
 
+export const fetchMeetups = (request) => {
+	console.log(request);
+	return axios({
+		method:'get',
+		url:"https://api.meetup.com/2/open_events/?key=682f5e4b26d16d31377034866e33",
+    params:{
+      city: request.city,
+			zipcode: request.zipcode,
+			radius: request.radius,
+      text: request.text,
+			time: request.time,
+			text_format: "plain"
+    }
+	}).then(response => console.log(response.data.results));
+};
+
+
+//example
 // export const fetchMeetups = (request) => {
 // 	return axios({
 // 		method:'get',
 // 		url:"https://api.meetup.com/2/open_events/?key=682f5e4b26d16d31377034866e33",
 //     params:{
-//       city: request.city,
-// 			zipcode: request.zipcode,
-// 			radius: request.radius,
-//       text: request.text,
-// 			time: request.time,
-// 			text_format: "plain"
+//       city: "San Francisco",
+// 			zipcode: 94015,
+// 			radius: 15.0,
+//       text: "tech AND code",
+// 			order: "time",
+// 			text_format:"plain"
 //     }
-// 	}).then(response => console.log(response.data.results));
+// 	});
 // };
-
-
-//example
-export const fetchMeetups = (request) => {
-	return axios({
-		method:'get',
-		url:"https://api.meetup.com/2/open_events/?key=682f5e4b26d16d31377034866e33",
-    params:{
-      city: "San Francisco",
-			zipcode: 94015,
-			radius: 15.0,
-      text: "tech AND code",
-			order: "time",
-			text_format:"plain"
-    }
-	});
-};
 
 // let epoch = new Date('2016-10-11').getTime();
 // console.log(epoch);
