800deb42713856f1e759dccda0bea960027aeaeb
Scott Heng <scottheng3@gmail.com>
2017-05-26 13:45:40 -0700

    add basic styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import SearchContainer from './search/search_container';
 import MeetupContainer from './meetup/meetup_container';
 import EventbriteContainer from './eventbrite/eventbrite_index_container';
-
+import IndexPage from './pages/index_page';
 
 const App = (props) => {
   return (
     <div>
-      <SearchContainer />
-      <MeetupContainer />
-      <EventbriteContainer />
+      <IndexPage />
     </div>
   );
 };
