51fe1b5a150bad957f117c20a1123897c0538433
Scott Heng <scottheng3@gmail.com>
2017-05-29 17:17:16 -0700

    sort eventbrite events by date

--- frontend/components/eventbrite/eventbrite_index.jsx
+++ frontend/components/eventbrite/eventbrite_index.jsx
@@ -5,12 +5,14 @@ import { timeStampParser } from '../../utils/timestamp_parser';
 class eventbriteIndex extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {events: []};
+		this.state = {events: [], currentDate: null};
 	}
 
 
 	render() {
 		// console.log(this.props.events);
+		let currentDate = null;
+
     if(this.props.events.length === 0 && Object.keys(this.props.meetupEvents).length === 0){
       return <div>Loading...</div>;
     }
@@ -21,19 +23,36 @@ class eventbriteIndex extends React.Component {
 				<ul className="events-list">
 					{this.props.events.map((event, idx) => {
 						const datetime = timeStampParser(event.start.local);
-						return (
-							<li key={idx} className="events-item flex-container flex-horizontal">
-								<div className="events-left">
-									<h4>Date: {datetime[0]}</h4>
-									<h4>Time: {datetime[1]}</h4>
+						if (datetime[0] === currentDate) {
+							return (
+								<li key={idx} className="events-item flex-container flex-horizontal">
+									<div className="events-left">
+										<h4>Date: {datetime[0]}</h4>
+										<h4>Time: {datetime[1]}</h4>
+									</div>
+									<div className="events-right">
+										<h3>{event.name.text}</h3>
+									</div>
+								</li>
+							);
+						}
+						else {
+							currentDate = datetime[0];
+							return (
+								<div>
+									<h1>{currentDate}</h1>
+									<li key={idx} className="events-item flex-container flex-horizontal">
+										<div className="events-left">
+											<h4>Date: {datetime[0]}</h4>
+											<h4>Time: {datetime[1]}</h4>
+										</div>
+										<div className="events-right">
+											<h3>{event.name.text}</h3>
+										</div>
+									</li>
 								</div>
-								<div className="events-right">
-									<h3>{event.name.text}</h3>
-								</div>
-								{/*<a href={event.url}>Go to Page</a>*/}
-								{/*<h4>{event.url}</h4>*/}
-							</li>
-						);
+							);						
+						}
 					})}
 				</ul>
 			</div>
--- frontend/components/pages/index_page.jsx
+++ frontend/components/pages/index_page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchIndexContainer from '../search/search_index_container';
 import MeetupContainer from '../meetup/meetup_container';
 import EventBriteContainer from '../eventbrite/eventbrite_index_container';
+// import NewEventbriteContainer from '../eventbrite/new_eventbrite_index_container';
 
 class IndexPage extends React.Component {
 	render() {
